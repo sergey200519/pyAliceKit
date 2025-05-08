79022a94d22db51e3e211852b0e825f5a31559e2
sergey200519 <oborok05@bk.ru>
2025-05-08 17:21:42 +0300

    reset

--- pyAlice/messages/reasons.py
+++
@@ -1,4 +0,0 @@
-reasons = {
-    "overwriting_default_text_event_resons-ru": "При записи message из KeyWord в default_text уже был текст, он бы перезаписан",
-    "from_key_words_event_resons-ru": "event был получен из KeyWord"
-}

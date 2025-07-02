1a0faac443d28ad12991a717b2762208be6c10e1
sergey200519 <oborok05@bk.ru>
2025-07-02 21:09:40 +0300

    style: deleting a file events.py

--- pyAliceKit/core/events.py
+++
@@ -1,8 +0,0 @@
-from types import ModuleType
-from typing import Self
-
-
-class Events:
-    def __init__(self: Self, settings: ModuleType) -> None:
-        self.__settings: ModuleType = settings
-        self.events = {}
